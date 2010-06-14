e9bcc80debff28e346ac675aab6d446ad98e582f
Christian Kraus <chrism-92@live.de>
2010-06-14 11:26:19 +0200

    Erster Test

---
+++ Konto.java
@@ -0,0 +1,22 @@
+package kraus;
+
+/**
+ * Die Kontoklasse mit dem eigentlichen Kontostand, im Konstruktor wird der Kontostand auf 1000 gesetzt.
+ * @author Christian Kraus
+ * @version 2010-04-11
+ */
+public class Konto {
+	private int kontostand;
+	/**
+	 * Setzt den Kontostand defaultm‰ﬂig auf 1000.
+	 */
+	public Konto () {
+		this.kontostand=1000;
+	}
+	public void setKonto (int kontostand) {
+		this.kontostand=kontostand;
+	}
+	public int getKonto () {
+		return this.kontostand;
+	}
+}
